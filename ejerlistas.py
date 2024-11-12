5cf0cfb5a0d22f2a7bcab50daba2286391210e6a
almengar <almengar326@gmail.com>
2024-11-12 17:55:27 +0100

    Add files via upload

--- ejerlistas.py
+++ ejerlistas.py
@@ -30,18 +30,14 @@ print(timeit.timeit(stmt='["red", "green", "blue"]', number=1000000000)) """
 # Teniendo una lista de caracteres [“k”, “y”, “e”, “f”, “i”], 
 # comprobar cada uno de los elementos para ver si es un vocal (vocales = [“a”, “e”, “i” …]). 
 # Mostrar True si es vocal y False si es constante.
-""" vocal = True
-listacaracter = ["k", "y", "e", "f", "i"]
+
+""" listacaracter = ["k", "y", "e", "f", "i"]
 listavocales = ["a", "e", "i", "o", "u"]
 for i in listacaracter:
-    for j in listavocales:
-        if listacaracter(i) == listavocales(j):
-            print(f"el caracter {listacaracter(i)} es una vocal")
-            print(vocal)
+        if i in listavocales:
+            print(f"el elemento {i}, True")
         else:
-            vocal = False
-            print(f"el caracter {listacaracter(i)} es una consonante")
-            print(vocal) """
+            print(False) """
 
 # vocales(correccion profesor)
 """ letras = ["a", "b", "e", "f", "o", "x", "c"]
@@ -59,18 +55,31 @@ for l in letras:
 # Probablemente traerán una o más tiendas. 
 # Al final, contar el número de tiendas que han traído todos.
 
-""" Ana = ["saco", "cantimplora"]
+""" Ana = ["saco", "cantimplora", "tienda"]
 Carlos = ["tienda", "comida"]
-Maria = ["botas", "ropa"]
+Maria = ["botas", "ropa", "tienda"]
 
-for i in Ana:
+equipamiento = Ana + Carlos + Maria """
+
+""" print(", ".join(equipamiento)) """ #para poder imprimir las listas seguidas separados de una coma
+
+""" print(Ana.count("tienda")) """  #cuenta las veces que aparece la tienda en la lista Ana
+""" for i in Ana:
     print(f"Equipamiento de Ana: {i}") 
 for j in Carlos:
     print(f"Equipamiento de Carlos: {j}") 
 for k in Maria:
     print(f"Equipamiento de Maria: {k}") 
 z = Ana + Carlos + Maria
-print(z) """
+print(z)
+cont = 0
+for i in z:
+    if i == "tienda":
+        cont = cont + 1
+
+print(f"la tienda aparece  {cont} veces")
+
+print(z.count("tienda")) """  # con esta sentencia se cuentan todos los elementos de la lista que son TIENDA
 
 #Ejercicio Camping(Version profesor)
 """ maria = ["tienda", "linterna", "saco de dormir"]
@@ -115,5 +124,82 @@ compras[4] = "zumo de naranja"
 print(compras)
 compras.remove("zumo de naranja")
 print(compras)
-compras.insert(2, "limones")
+compras.insert(2, "limones")   #Se añade un elemento en el centro de la lista
+compras.sort()
 print(compras)
+
+#*****************************************************************************************
+
+""" frutas = ["kiwi", "manzana", "naranja"]
+for fruta in frutas:
+    print(fruta)
+
+frutas.append("mandarina")
+
+for fruta in frutas:
+    print(fruta)
+
+x, *y = frutas   #coge primero el primer elemento y luego el resto de elementos de la lista
+*x, y, z = frutas
+print(x)
+print(y)
+#print(dir(frutas)) """
+#*****************************************************************************************
+""" 
+rgb = (145, 54, 11)
+coordenadas = (123, 5)  # coordenadas X e Y """
+
+
+# Gestión de una tienda
+# Se podría extender la idea de arriba para crear una aplicación 
+# para gestionar la compra de productos en una tienda.
+
+ProductosEspeciales = ["Manzanas", "Zumos", "Leche"]
+Productos = ["Huevos", "Lechuga", "Atún"]
+TotalProductos = ProductosEspeciales + Productos
+
+print("**************MENU**************")
+print("opcion 1: Ver todos los productos")
+print("opcion 2: Hacer una compra")
+print("opcion 3: Borrar un producto")
+print("********************************")     
+
+opcion = int(input("Elige opción"))
+
+if opcion == 1:
+    print("\n".join(TotalProductos))
+elif opcion == 2:
+    print("********************************") 
+    for i in TotalProductos:
+        if i in ProductosEspeciales:
+            print(f"{i}: precio 10 euros/kilo")
+        else:
+            print(f"{i}: precio 12 euros/kilo")
+    
+    print("********************************")  
+
+    importe = float(input("Introduzca el importe que va a dar"))
+    total = 0
+    seguir = true
+     
+    compra = input("¿Qúe producto vas a comprar?")
+
+    if compra in TotalProductos:
+        print("El producto esta en el listado")
+        if compra in ProductosEspeciales:
+            total = total + 10
+        else: 
+            total = total + 12
+    else:
+        print("El producto no esta en el listado")
+elif opcion == 3:
+    producto_eliminar = input("¿Qúe producto vas a eliminar?")
+    if producto_eliminar in TotalProductos:
+        print("producto eliminado")
+        TotalProductos.remove(producto_eliminar)
+        print(TotalProductos)
+    else:
+        input("No tenemos este producto")
+       
+
+    
