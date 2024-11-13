9434965e321f26e6805282140ab16b664e08053e
almengar <almengar326@gmail.com>
2024-11-13 16:59:37 +0100

    Add files via upload

---
+++ pilalibros.py
@@ -0,0 +1,20 @@
+# Gestión de una pila de libros
+# Crear una aplicación para gestionar una pila de libros en una biblioteca. 
+# Simular las acciones de push, pop y peek. 
+# Por ejemplo, preguntar al usuario por un nombre de un libro, y lo agregas a la pila de libros. 
+# Si alguien quiere un libro en particular, tendrás que sacar todos los libros que están encima, antes de dárselo.
+
+
+
+
+""" seguir = True
+libros = [ ]
+
+libro = input("¿Que libro quieres añadir? ")
+libros.append(libro)
+print(libro)
+
+libros.pop(-1)   #Elimina el elemento de más arriba de la pila """
+
+x = 4.5
+print(isinstance(x, int))
