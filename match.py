a2f294b418ac29bdf620d13c141dc24b1a571f65
almengar <almengar326@gmail.com>
2024-11-20 14:54:45 +0100

    Add files via upload

---
+++ match.py
@@ -0,0 +1,9 @@
+color = "rojo"
+
+match color:
+    case "rojo":
+        print("tu color favorito es el rojo")
+    case "azul":
+        print("tu color favorito es el azul")
+    case _:
+        print("No tenemos ese color")
